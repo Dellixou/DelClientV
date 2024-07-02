5f5a8dbb5ee985a2200dc7ba53b7587c6992dd36
GitHub Action <action@github.com>
2024-07-02 08:43:58 +0000

    Initialize template repository proper
    Set package name to com.github.dellixou.delclientv3
    Set project name to delclientv3
    Set modid to delclientv

--- src/main/java/com/github/dellixou/delclientv3/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/dellixou/delclientv3/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.dellixou.delclientv3.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/dellixou/delclientv3/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/dellixou/delclientv3/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.dellixou.delclientv3.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
